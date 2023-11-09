5f45f2b5e50469fdb1a5555a2a437f89beacb683
Nick <nickjpanella@gmail.com>
2023-11-09 01:05:47 -0500

    callback fix

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -3,6 +3,13 @@ import { defineSecret } from "firebase-functions/params";
 
 export const app = initializeApp();
 
+const devCallback = import.meta.env.VITE_CALLBACK_URL;
+const devCallbackValue = {
+  value: () => devCallback,
+};
+
 export const DISCORD_CLIENT_ID = defineSecret("DISCORD_CLIENT_ID");
 export const DISCORD_CLIENT_SECRET = defineSecret("DISCORD_CLIENT_SECRET");
-export const DISCORD_CALLBACK_URL = defineSecret("DISCORD_CALLBACK_URL");
+export const DISCORD_CALLBACK_URL = devCallback
+  ? devCallbackValue
+  : defineSecret("DISCORD_CALLBACK_URL");
--- functions/vite.config.ts
+++ functions/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
     }),
   ],
   envDir: "../",
-  envPrefix: "FUNC_",
   build: {
     outDir: "../dist/functions",
     emptyOutDir: true,
